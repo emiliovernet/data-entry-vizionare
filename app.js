ce29cb771c4c4557306496cb582eb19b11542018
Emilio Vernet <vernetemilio@gmail.com>
2023-12-09 23:56:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+window.onload = () => {
+    fetch(
+        "https://script.google.com/macros/s/AKfycbwHPgffOPRiqSMTK4TWTbVXybyIK_mqQSQ8IkzaThsIqScyzbRdiqqUXbBRS1viVsA/exec?sheetName=Sheet2"
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            // Obtener la lista de nombres y operaciones
+            const nombres = data.slice(1).map((row) => row[0]);
+            const operaciones = data.slice(1).map((row) => row[1]);
+
+            // Llenar el campo de selección de nombres
+            const selectNombre = document.getElementById("Nombre");
+            nombres.forEach((nombre) => {
+                const option = document.createElement("option");
+                option.value = nombre;
+                option.textContent = nombre;
+                selectNombre.appendChild(option);
+            });
+
+            // Llenar el campo de selección de operaciones
+            const selectOperacion = document.getElementById("Operacion");
+            operaciones.forEach((operacion) => {
+                const option = document.createElement("option");
+                option.value = operacion;
+                option.textContent = operacion;
+                selectOperacion.appendChild(option);
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching names and operations:", error);
+        });
+
+    document.getElementById("form").addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent the default form submission
+        document.getElementById("message").textContent = "Enviando..";
+        document.getElementById("message").style.display = "block";
+
+        // Collect the form data
+        var formData = new FormData(this);
+        var keyValuePairs = [];
+        for (var pair of formData.entries()) {
+            keyValuePairs.push(pair[0] + "=" + pair[1]);
+        }
+
+        var formDataString = keyValuePairs.join("&");
+
+        // Send a POST request to your Google Apps Script
+        fetch(
+            "https://script.google.com/macros/s/AKfycbwHPgffOPRiqSMTK4TWTbVXybyIK_mqQSQ8IkzaThsIqScyzbRdiqqUXbBRS1viVsA/exec",
+            {
+                redirect: "follow",
+                method: "POST",
+                body: formDataString,
+                headers: {
+                    "Content-Type": "text/plain;charset=utf-8",
+                },
+            }
+        )
+            .then(function (response) {
+                // Check if the request was successful
+                if (response) {
+                    return response; // Assuming your script returns JSON response
+                } else {
+                    throw new Error("Failed to submit the form.");
+                }
+            })
+            .then(function (data) {
+                // Display a success message
+                document.getElementById("message").textContent =
+                    "Envío de datos exitoso";
+                document.getElementById("message").style.display = "block";
+                document.getElementById("message").style.backgroundColor = "green";
+                document.getElementById("message").style.color = "beige";
+                document.getElementById("form").reset();
+
+                setTimeout(function () {
+                    document.getElementById("message").textContent = "";
+                    document.getElementById("message").style.display = "none";
+                }, 2600);
+            })
+            .catch(function (error) {
+                // Handle errors, you can display an error message here
+                console.error(error);
+                document.getElementById("message").textContent =
+                    "An error occurred while submitting the form.";
+                document.getElementById("message").style.display = "block";
+            });
+    });
+};
+
